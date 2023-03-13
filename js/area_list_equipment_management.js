47976d8ff24a3ad3c6a2a8b9ec825ae57c29fb75
hdqphu <hdqphu.dev@gmail.com>
2023-03-13 17:03:06 +0700

    create area list equipment management screen

---
+++ js/area_list_equipment_management.js
@@ -0,0 +1,21 @@
+import * as Common from "./common_function.js"
+import * as Mess from "./message.js"
+import * as StringCS from "./string.js"
+
+/*
+   * ONCLICK ACTION
+*/
+function onClickAction () {
+    document.getElementById("eriaIchiran").onclick = function () {
+        Common.movePage("/equipment_management_list.html")
+    }
+}
+
+/*
+   * ONLOAD ACTION
+*/
+function onLoadAction() {
+    onClickAction();
+}
+
+window.onload = onLoadAction;
