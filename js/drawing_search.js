39191370cca33cfe4d8a9e8df4a7a2439ebc87a8
hdqphu <hdqphu.dev@gmail.com>
2023-03-13 15:45:12 +0700

    check date function

--- js/drawing_search.js
+++ js/drawing_search.js
@@ -1,19 +1,41 @@
 import * as Common from "./common_function.js"
+import * as Mess from "./message.js"
+import * as StringCS from "./string.js"
 
 /**
    * ONCLICK ACTION
 */
-function onclickAction() {
+function onClickAction() {
 	document.getElementById("ichiranHyojiBtn").onclick = function () {
-        Common.movePage("/drawing_display.html")
-    }
+      Common.movePage("/drawing_display.html")
+   }
 }
 
+/**
+   * ONCHANGE ACTION
+*/
+function onChangeAction() {
+	document.getElementById("shuryo").onchange = function () {
+      checkValidDay();
+   }
+}
+
+/**
+   * CHECK VALID DATE
+ */
+function checkValidDay() {
+   var startDay = document.getElementById("kaishi").value;
+   var endDay = document.getElementById("shuryo").value;
+   if (endDay < startDay) {
+      Common.setupModal("error", null, Mess.I00008, StringCS.CLOSE, null, null, false);
+   }
+}
 /**
    * ONLOAD ACTION
 */
 function onLoadAction() {
-	onclickAction();
+	onClickAction();
+   onChangeAction();
 }
 
 window.onload = onLoadAction;
